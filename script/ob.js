671169dcc1ad18fe48e06820daebcc9160228715
lolkagirl <127850230+lolkagirl@users.noreply.github.com>
2023-12-05 00:40:00 +0300

    Add files via upload

---
+++ script/ob.js
@@ -0,0 +1,35 @@
+const o10 = document.getElementById("o10");
+const o30 = document.getElementById("o30");
+const o50 = document.getElementById("o50");
+
+o10.onclick = () => {
+    if (o50.style = "#404B34") {
+        o10.style.color = "#404B34";
+        o50.style.color = "#darkgray";
+    }
+    if (o30.style = "#404B34") {
+        o10.style.color = "#404B34";
+        o30.style.color = "#darkgray";
+    }
+    
+}
+o30.onclick = () => {
+    if (o50.style = "#404B34") {
+        o30.style.color = "#404B34";
+        o50.style.color = "#darkgray";
+    }
+    if (o10.style = "#404B34") {
+        o30.style.color = "#404B34";
+        o10.style.color = "#darkgray";
+    }
+}
+o50.onclick = () => {
+    if (o30.style = "#404B34") {
+        o50.style.color = "#404B34";
+        o30.style.color = "#darkgray";
+    }
+    if (o10.style = "#404B34") {
+        o50.style.color = "#404B34";
+        o10.style.color = "#darkgray";
+    }
+}
